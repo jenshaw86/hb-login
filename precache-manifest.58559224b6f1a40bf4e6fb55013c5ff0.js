919a6fbf785eeabc910696998df384ed1d3b439e
jenshaw86 <bloo.j.shaw@gmail.com>
2019-12-06 00:45:55 -0800

    Updates

---
+++ precache-manifest.58559224b6f1a40bf4e6fb55013c5ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71641aae3d56f072e56e52e32f554fb6",
+    "url": "/hb-login/index.html"
+  },
+  {
+    "revision": "dee198bc698a1535166a",
+    "url": "/hb-login/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2dba2638904a5a949b15",
+    "url": "/hb-login/static/css/main.0779a3d7.chunk.css"
+  },
+  {
+    "revision": "dee198bc698a1535166a",
+    "url": "/hb-login/static/js/2.ce8cd136.chunk.js"
+  },
+  {
+    "revision": "2dba2638904a5a949b15",
+    "url": "/hb-login/static/js/main.6b2d71ac.chunk.js"
+  },
+  {
+    "revision": "b9a8790f83342f5b9958",
+    "url": "/hb-login/static/js/runtime-main.f4db26e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hb-login/precache-manifest.7de2938c23513eccefc977a39bc575c3.js"
+  "/hb-login/precache-manifest.58559224b6f1a40bf4e6fb55013c5ff0.js"
 );
 
 self.addEventListener('message', (event) => {
